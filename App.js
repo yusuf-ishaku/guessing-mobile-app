260cf6c22be3be034ef8d061a29e7c142c769431
yusuf <kingshakzy@gmail.com>
2023-04-16 19:24:56 +0100

    Errors wan kill me

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Header } from './components/Header';
@@ -6,25 +6,43 @@ import { StartGameScreen } from './screens/StartGameScreen';
 import { Card } from './components/Card';
 import { GameScreen } from './screens/GameScreen';
 
-import { AppLoading } from 'expo'
+// import AppLoading from 'expo-app-loading';
+import * as SplashScreen from 'expo-splash-screen';
 import * as Font from "expo-font";
 
-const fetchFonts = () =>{
-  Font.loadAsync({
-    'open-sans': require('./assets/fonts/OpenSans-Bold.ttf'),
-    // 'open-sans-bold': require('./assets/fonts/OpenSans-Regular.ttf')
-  })
+const fetchFonts = async () =>{
+  Font.loadAsync(
+   './assets/fonts/OpenSans-Bold.ttf'
+    // 'open-sans-bold': require('./assets/fonts/OpenSans-Regular.ttf'))
+  )
 }
-
+SplashScreen.preventAutoHideAsync()
 
 export default function App() {
   const [userNumber, setUserNumber] = useState();
-  const [fontsLoaded, setFontloaded] = useState(false)
+  const [fontsLoaded, setFontloaded] = useState(false);
   
-  if(!fontsLoaded){
-    return <AppLoading startAsync ={fetchFonts} onFinish = {() =>{setFontloaded(true)}}></AppLoading>
-  }
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Pre-load fonts, make any API calls you need to do here
+        await fetchFonts()
+        // Artificially delay for two seconds to simulate a slow loading
+        // experience. Please remove this if you copy and paste the code!
+        // await new Promise(resolve => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setFontloaded(true);
+      }
+    }
 
+    prepare();
+  }, []);
+  if(fontsLoaded){
+    return SplashScreen.hideAsync();
+  }
   const startGameHandler = (selectedNumber) =>{
     setUserNumber(selectedNumber)
   }
